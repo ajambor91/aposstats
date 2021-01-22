8dfb3983d584cb3ea6816e68241bad143a8bed65
Adam <adam91mko@gmail.com>
2021-01-22 20:27:00 +0100

    available administration units

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,24 +1,25 @@
 <?php
+
 namespace App\Controller;
 
 use App\Entity\Apostasy;
 use App\Entity\AppConfig;
 use App\Repository\ApostasyRepository;
 use App\Repository\AppConfigRepository;
+use App\Repository\CityRepository;
 use App\Repository\VoivodeshipRepository;
-use App\Service\FitCities;
-use App\Service\MergeTerytData;
+use App\Service\PrepareAdministrationsUnitData;
 use App\Service\PrepareApostasiesResponse;
-use App\Service\Scrapper;
-use App\Service\Teryt;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 /**
  * @Route("/")
  */
-class MainController extends AbstractController {
+class MainController extends AbstractController
+{
 
     /**
      * @Route("/", methods={"GET"}, name="main_page")
@@ -31,20 +32,52 @@ class MainController extends AbstractController {
      */
 
     public function showIndex(Request $request,
-                              ApostasyRepository  $apostasyRepository,
+                              ApostasyRepository $apostasyRepository,
                               PrepareApostasiesResponse $apostasiesResponse,
-                              AppConfigRepository $configRepository): JsonResponse {
+                              AppConfigRepository $configRepository): JsonResponse
+    {
 
         $data = $request->query->all();
         if (isset($data['period']) && $data['period'] === Apostasy::BY_MONTH) {
             $startDate = $configRepository->findOneBy(['configKey' => AppConfig::CONFIG_KEYS[AppConfig::START_DATE]]);
-            if(new \DateTime($data['from']) < new \DateTime($startDate)){
+            if (new \DateTime($data['from']) < new \DateTime($startDate)) {
                 $data['from'] = $startDate;
             }
         }
         $result = $apostasyRepository->getApostasiesData($data);
         $result = $apostasiesResponse->prepareData($result);
         return new JsonResponse($result, 200);
+    }
+
+    /**
+     * @Route("/get-available-voivodeships", methods={"GET"}, name="get_available_voivodeships")
+     * @param VoivodeshipRepository $voivodeshipRepository
+     * @param PrepareAdministrationsUnitData $administrationsUnitData
+     * @return JsonResponse
+     */
+    public function getAvailableVoivodeships(VoivodeshipRepository $voivodeshipRepository,
+                                             PrepareAdministrationsUnitData $administrationsUnitData): JsonResponse
+    {
+        $data = $voivodeshipRepository->findAll();
+        $data = $administrationsUnitData->prepareVoivodeships($data);
+        return new JsonResponse($data, 200);
+    }
+
+    /**
+     * @Route ("/get-available-cities", methods={"GET"}, name="get_available_cities")
+     * @param Request $request
+     * @param CityRepository $cityRepository
+     * @param PrepareAdministrationsUnitData $administrationsUnitData
+     * @return JsonResponse
+     */
+    public function getAvailableCities(Request $request,
+                                       CityRepository $cityRepository,
+                                       PrepareAdministrationsUnitData $administrationsUnitData): JsonResponse
+    {
+        $voivodeship = $request->query->get('voivodeshipId');
+        $data = $cityRepository->findBy(['voivodeship' => $voivodeship]);
+        $data = $administrationsUnitData->prepareCities($data);
+        return new JsonResponse($data, 200);
     }
 
 }
---
+++ src/Service/PrepareAdministrationsUnitData.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\City;
+use App\Entity\Voivodeship;
+
+class PrepareAdministrationsUnitData
+{
+    public function prepareCities(array $data): array
+    {
+        $result = [];
+        /**
+         * @var City $datum
+         */
+        foreach ($data as $datum) {
+            $result = [
+                'id' => $datum->getId(),
+                'name' => $datum->getName()
+            ];
+        }
+        return $result;
+    }
+
+    public function prepareVoivodeships(array $data): array
+    {
+        $result = [];
+        /**
+         * @var Voivodeship $datum
+         */
+        foreach ($data as $datum) {
+            $result[] = [
+                'id' => $datum->getId(),
+                'name' => ucfirst(mb_strtolower($datum->getName()))
+            ];
+        }
+        return $result;
+    }
+}
